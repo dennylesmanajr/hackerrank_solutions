29ca9246b7b9c0f274f2a24d7650b9b1d50f2072
Denny Lesmana <denny.lesmana@treessolutions.com>
2021-01-22 00:53:08 +0700

    update solution new year chaos

--- Interview Preparation Kit/Arrays/NewYearChaos.js
+++ Interview Preparation Kit/Arrays/NewYearChaos.js
@@ -28,13 +28,12 @@ function minimumBribes(q) {
     let bribes = 0;
     let chaotic = false;
     for (let i = 0; i < q.length; i++) {
-        
         if(q[i] -(i+1)>2) {
             chaotic = true;
             break;
         }
         
-        for (let j = 0; j < i; j++) {
+        for (let j = q[i]-2; j < i; j++) {
             if(q[j] > q[i]) bribes++;
         }
     }
